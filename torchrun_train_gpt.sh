2f9379a021e106e5c2020ced854417fa166768be
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2025-09-23 23:35:49 +0000

    upgrade to cuda 12.9 as the nccl uses that library

--- torchrun_train_gpt.sh
+++ torchrun_train_gpt.sh
@@ -2,6 +2,10 @@ export OMP_NUM_THREADS=24
 export OMP_PROC_BIND=SPREAD
 export OMP_PLACES=CORES
 
+export NCCL_DEBUG=INFO
+export NCCL_P2P_LEVEL=NVL
+export CUDA_DEVICE_MAX_CONNECTIONS=1      # often helps throughput
+
 torchrun \
     --standalone \
     --nproc_per_node=8 \
